444d77838bd2321a39320a29b76ca6667ffd9275
Raphael Beutel <rb@gobasil.com>
2012-11-09 16:59:16 +0100

    Added theme Base Files

---
+++ themes/Amano/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+	<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+	<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+	<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+	<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title><?php the_title(); ?></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+		
+		<link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="css/reset.css">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/grid.css">
+        
+        <script language="JavaScript" src="http://code.jquery.com/jquery-1.8.2.js">
+    </head>
+    <body>
