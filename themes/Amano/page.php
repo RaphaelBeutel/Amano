5d7a230929d67e371ae80085f2998705bac03d75
Raphael Beutel <rb@gobasil.com>
2012-11-11 19:17:44 +0100

    Added Amano page

--- themes/Amano/page.php
+++ themes/Amano/page.php
@@ -1,3 +1,11 @@
 <?php get_header(); ?>
-
+		<div class="wrapper">
+			<article id="page">
+				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+					
+					<?php the_content(); ?>
+					
+				<?php endwhile; endif; ?>
+			</article>
+		</div>
 <?php get_footer(); ?>
