38d5c3ee1daad92671b5616889bd7941ecbfcd50
Raphael Beutel <rb@gobasil.com>
2012-11-15 23:35:25 +0100

    Added blog

--- themes/Amano/blog.php
+++ themes/Amano/blog.php
@@ -1,4 +1,30 @@
 <?php /* Template Name: Blog */ ?>
 <?php get_header(); ?>
-	asfdasdf
+	<div class="wrapper">
+		<article id="blog">
+			
+			<h1>Blog</h1><hr style="margin-bottom:40px;" /> 
+			
+			<?php $post_number = 0; ?>
+			
+			<?php query_posts( 'post_type=post' ); ?>
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+				<?php if ( $post_number == 0 ) { ?>
+					<div id="<?php the_ID(); ?>" class="blog close open">
+				<?php }else{ ?>
+					<div id="<?php the_ID(); ?>" class="blog close ">
+				<?php } ?>
+					<div id="title-<?php the_ID(); ?>" class="title">
+						<img id="arrow" src="<?php bloginfo('template_directory'); ?>/gfx/arrow.png">
+						<h2><?php the_title(); ?></h2>
+					</div>
+					<div id="content-<?php the_ID(); ?>" class="content">
+						<?php echo the_content(); ?>
+					</div>
+					
+				</div>
+				<?php $post_number++; ?>
+			<?php endwhile; endif; ?>
+		</article>
+	</div>
 <?php get_footer(); ?>
